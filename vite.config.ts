ef01c3fdacf1937c21a2e5c2b6ee9f9b4ff63aeb
phankimhue105-del <phankimhue105@gmail.com>
2026-07-29 22:13:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// cache-invalidation-timestamp: 2026-07-29-21-59
+// cache-invalidation-timestamp: 2026-07-29-22-10
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
