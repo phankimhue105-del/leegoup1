9ce9f23ca884474ebc4a9ca25c0e2c562a888fff
phankimhue105-del <phankimhue105@gmail.com>
2026-08-02 21:20:11 +0700

    Update lesson2.ts

--- src/data/practice/unit4/lesson2.ts
+++ src/data/practice/unit4/lesson2.ts
@@ -74,7 +74,7 @@ export const lesson2Practice: PracticeQuestion[] = [
   {
     id: 6,
     type: "multiple_choice",
-    question: "### TEST U4-L2 ###",
+    question: "Do you like chicken?",
     image: "🙁🍗",
     vocabulary: "chicken",
     choices: ["Yes, I do.", "No, I don't.", "Yes, it is.", "No, it isn't."],
